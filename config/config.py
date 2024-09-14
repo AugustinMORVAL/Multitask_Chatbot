3f1936d492d0b5db1f4b3b41d38d970e7932b77a
Augustin MORVAL <augustin.morval.pro@gmail.com>
2024-09-14 14:48:26 +0200

    adding parameters

--- config/config.py
+++ config/config.py
@@ -1,5 +1,13 @@
+import os
 import yaml
 
-def load_config(config_path='config.yaml'):
-    with open(config_path, 'r') as config_file:
-        return yaml.safe_load(config_file)
+def load_config():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(current_dir, 'config.yaml')
+    
+    try:
+        with open(config_path, 'r') as config_file:
+            return yaml.safe_load(config_file)
+    except FileNotFoundError:
+        print(f"Config file not found at {config_path}")
+        return {}
